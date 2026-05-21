1de0ade2804af129acc04e1be6e2c89d276acf7b
abdul Rafay <abdulrafay700000@gmail.com>
2026-05-21 23:01:16 +0500

    Eager Loading

--- app/Http/Controllers/readController.php
+++ app/Http/Controllers/readController.php
@@ -8,7 +8,11 @@ use App\Models\insert;
 class readController extends Controller
 {
     function Read(){
-        $data = insert::paginate(4);
+
+    // Acha Ab ye select isliye use hota he jaab hamhe database se sirf kaam ke 
+    // ke columns mangwane ho faltu nhi is se speed increase hoti he 
+
+        $data = insert::select('id', 'Name', 'Email')->paginate(2);
 
         return view('LaravelDatabase.read', ['data' => $data]);
     }
