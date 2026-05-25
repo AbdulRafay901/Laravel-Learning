95737f449aa454c55307b27f13728584e6f7f831
abdul Rafay <abdulrafay700000@gmail.com>
2026-05-26 03:25:41 +0500

    Debugging and Error Handling implement

--- app/Http/Controllers/fetchController.php
+++ app/Http/Controllers/fetchController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class fetchController extends Controller
 {
-    public function fetch(){
-        $users =  DB::select('SELECT * FROM users');
-        return view('database.fetch', compact('users'));
-        
+    public function fetch()
+    {
+        try {
+
+            $users =  DB::select('SELECT * FROM users');
+
+            return view('database.fetch', compact('users'));
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+
+            // Acha ye log error isliye use hota he jaab bhi koi error aye wo
+            // storage\log\laravel.log file me save hojaye
+            // Mtlb ham sorahe hon or website me koi error ajaye to wo
+            // is file me save hojayega ham utha kr check krenge to pata chal jayega
+        }
+
     }
 }
